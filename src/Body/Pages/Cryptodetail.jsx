a843b197542e47cca4ff8bb6484223c460974afd
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2024-03-13 13:40:18 +0530

    updet evaluation

--- src/Body/Pages/Cryptodetail.jsx
+++ src/Body/Pages/Cryptodetail.jsx
@@ -1,25 +1,44 @@
-import { Box ,Text} from '@chakra-ui/react'
+import { Box ,Heading,Text} from '@chakra-ui/react'
 import React, { useEffect ,useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { useGetCryptosQuery } from '../../Servisec/CryptoApi'
+import millify from 'millify'
 
 export default function Cryptodetail() {
 const {coin_id} = useParams()
 const { data, isFetching, isLoading,status} = useGetCryptosQuery(100)
 
 const [crypto, setcrypto] = useState(null)
+const [serchedData, setserchedData] = useState(null)
 
-// console.log(data?.data);
-if(data?.data?.name === coin_id){
-  console.log(data?.data);
+crypto?.filter((coin)=>{
+if(coin.name=== coin_id){
+ setserchedData(coin);
 }
+})
+
+
 
 useEffect(()=>{
+  setcrypto(data?.data?.coins)
+},[status])
 
-},[data])
+const {
+  rank,price,iconurl,btcPrice,change,marketCap
+} = serchedData
+
   return (
-    <Box border={'solid 2px red'} h={'380px'} >Cripto Ditail
-     <Text>{coin_id}</Text>
+    <Box border={'solid 2px red'} h={'380px'} >
+     <Heading> Cripto Currecies Ditail</Heading>
+     <Box>
+     <Image src={crypto?.iconurl}/>
+     </Box>
+   <Text>{crypto?.name}</Text>
+   {/* <Text>Ranking : {crypto?.rank}</Text> */}
+   <Text>Price : {millify(crypto?.price)}</Text>
+   <Text>Btc Price : {millify(crypto?.btcPrice)}</Text>
+   <Text>Market Cap : {millify(crypto?.marketCap)}</Text>
+   
      </Box>
   )
 }
--- src/Body/Pages/Exchangepage.jsx
+++ src/Body/Pages/Exchangepage.jsx
@@ -1,7 +1,42 @@
 import { Box } from '@chakra-ui/react'
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import axios from 'axios';
 export default function Exchangepage() {
+
+
+  const options = {
+    method: 'GET',
+    url: 'https://coinranking1.p.rapidapi.com/coins',
+    params: {
+      referenceCurrencyUuid: 'yhjMzLPhuIDl',
+      limit: '50',
+      offset: '0',
+      orderBy: '24hVolume',
+      orderDirection: 'desc',
+      rank:5
+    },
+    headers: {
+      'X-RapidAPI-Key': 'c378d16cd2msh888095c9d837338p17eb1ajsnca927d51c7ba',
+      'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+    }
+  };
+  
+  
+  const getData = async ()=>{
+    
+    try {
+      const response = await axios(options);
+      console.log(response?.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  
+  useEffect(()=>{
+  getData()
+  },[])
+
   return (
     <Box border={'solid 2px red'} h={'380px'} >Exchange</Box>
   )
